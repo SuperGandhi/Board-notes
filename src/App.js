43774e8542dbf228e4c87381a7c19543a5da9f9c
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-17 14:02:12 -0500

    use library icons and structure app building

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Tasks from './components/Tasks';
 import dogebeta from './images/dogebeta.png'
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
             className="betadoge-logo"
             alt ="dogebeta"
             />
+      </div>
+      <div className="works-main-list">
+          <h1>My tasks</h1>
+          <Tasks text='Learn React'/>
       </div>
     </div>
   );
